8337561ebb1b3e1b2f83c594eb5834edb1fdabe6
Katie <katie.mcy@outlook.com>
2023-05-15 12:16:19 -0400

    added back to home button

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const About = () => {
     return (
         <section className="about">
@@ -6,6 +8,7 @@ const About = () => {
                 <p>Moon Child is an astrology app that helps users explore the connection between their birthday and the moon's phases. By selecting their birth date on the home page, users can discover what kind of Moon Child they are, including whether the moon was waxing or waning, full or new, and other phases.</p>
                 <p>In the future, Moon Child will also include a feature where users can generate quotes and share them with others who share the same moon phase. This feature is currently under development and will be added to the app in a future update.</p>
                 <p>We hope you enjoy your Moon Child journey and find the app helpful in exploring your connection to the cosmos.</p>
+                <Link to ="/" className='bkHome'>Back to Home</Link>
             </div>
         </ section>
     )
